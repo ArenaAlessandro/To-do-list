e5c8a59b48cda71ecc000413eddf9f0d972ef3d5
CH0CL0 <arenaalessandro156@gmail.com>
2026-03-30 20:25:46 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import { useState, useEffect, use } from "react";
-
-function Boton() {
-  const [cantveces, setCantveces] = useState(0);
-  function Contar(){
-    setCantveces(cantveces +1)
-  }
-  return (
-    <div>
-      <button onClick={Contar}>hola</button>
-      <h2>me tocaste {cantveces} veces</h2>
-    </div>
-  )
-}
+import './App.css'
 
 export default function TodoList() {
       const [tarea, setTarea] = useState("");
       const [tareas, setTareas] = useState([]);
+      const [mensaje, setMensaje] = useState("");
+      const [error, setError] = useState(false);
 
       function AgregarTarea(){
           if (tarea === ""){
-          alert("pone algo dale")
+          setMensaje("Error al cargar. Nombre de tarea incompleta.")
+          setError(true)
         }else{
         setTareas([...tareas,{ id: Date.now(), nombre: tarea }]);
         setTarea("");
+        setMensaje("Tarea guardada correctamente.")
+        setError(false)
         }
       }
 
       function EliminarTarea(id){
         setTareas(tareas.filter((t) => t.id !== id));
+        setMensaje("Tarea eliminada correctamente.")
+        setError(false)
       }
   return (
-    <div>
-      <h1>LISTA</h1>
-      <input type="text" placeholder="agregar tarea" value={tarea} onChange={(e) => setTarea(e.target.value)}></input>
-      <button onClick={AgregarTarea}>Agregar tarea</button>
+    <div className="container">
+    <h1>LISTA</h1>
+      <div className="input-group">
+        <input type="text" placeholder="agregar tarea" value={tarea} onChange={(e) => setTarea(e.target.value)}></input>
+        <button onClick={AgregarTarea}>Agregar tarea</button>
+      </div>
+      {mensaje && (
+        <p className={error ? "error" : "cargado"}>{mensaje}</p>
+      )}
       <ul>{tareas.map((t) => (<li key={t.id}>{t.nombre} <button onClick={() => EliminarTarea(t.id)}>❌</button></li>))}</ul>
     </div>
   );
